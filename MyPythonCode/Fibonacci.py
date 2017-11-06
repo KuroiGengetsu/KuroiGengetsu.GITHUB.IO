eeea41c6e8bb20f1e86407af0596030432d254e0
Kuroi Gengetsu <33370369+KuroiGengetsu@users.noreply.github.com>
2017-11-06 20:12:14 +0800

    Add a python file

---
+++ MyPythonCode/Fibonacci.py
@@ -0,0 +1,11 @@
+"""Define a function to figure out Fibonacci numbers"""
+
+def Fibonacci(num):
+    """Use recursion 同figure out Fibonacci"""
+    if num == 1 or num == 2:
+        return 1
+    elif num > 2:
+        return Fibonacci(num - 1) + Fibonacci(num - 2)
+
+if __name__ == '__main__':
+    print(Fibonacci(13))
